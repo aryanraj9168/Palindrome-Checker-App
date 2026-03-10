7493bc18e9252bf7b4f9d266250422cef52181dc
Aryan Raj <aryanraj9168@gmail.com>
2026-03-10 21:51:32 +0530

    Update and rename welcome to UseCase5PalindromeCheckerApp.java

---
+++ UseCase5PalindromeCheckerApp.java
@@ -0,0 +1,33 @@
+import java.util.Stack;
+
+public class UseCase5PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        // Input string
+        String input = "noon";
+
+        // Create stack to store characters
+        Stack<Character> stack = new Stack<>();
+
+        // Push each character of the string into the stack
+        for (char c : input.toCharArray()) {
+            stack.push(c);
+        }
+
+        // Assume palindrome initially
+        boolean isPalindrome = true;
+
+        // Compare characters by popping from stack
+        for (char c : input.toCharArray()) {
+            if (c != stack.pop()) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        // Print result
+        System.out.println("Input : " + input);
+        System.out.println("Is Palindrome? : " + isPalindrome);
+    }
+}
